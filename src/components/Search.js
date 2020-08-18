fa0d17f0ab5ed34f1642b2b2ed3f8c885cad238f
penholder <penhold3r@gmail.com>
2020-08-18 19:02:27 -0300

    minor ui fixes

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,7 +10,7 @@ const Search = ({ shows, setSearchResults }) => {
 
 		if (searchValue) {
 			// filter by partial match in show's name
-			const results = shows.filter(({ show }) => show.name.toLowerCase().includes(searchValue))
+			const results = shows.filter(show => show.name.toLowerCase().includes(searchValue))
 			// pass results to parent
 			setSearchResults({ searchValue, results })
 			// clear form
